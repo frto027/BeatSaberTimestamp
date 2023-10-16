ebcaaada975ffd40813a01703d32d9d8fb2da507
frto027 <602706150@qq.com>
2023-10-16 23:09:38 +0800

    update author info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BSTimestamp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Display a timestamp on game scene to measure device delay.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BSTimestamp")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © frto027 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
